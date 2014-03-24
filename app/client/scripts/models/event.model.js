596149ad888eb6783f628687580cc244628a0681
David Tsuji <dts@sitecore.net>
2014-03-24 15:00:45 +1000

    updated the event model

--- app/client/scripts/models/event.model.js
+++ app/client/scripts/models/event.model.js
@@ -4,19 +4,28 @@ module.exports = cmi.class.model.extend(function () {
 
 	this.model = new cmi.modules.model('event')
 		.base(cmi.config.api.base)
-		.property('hash', {
-			type: 'string'
-		})
 		.property('name', {
 			type: 'string'
 		})
-		.property('when', {
+		.property('date', {
+			type: 'string',
+			default: ''
+		})
+		.property('time', {
 			type: 'string',
 			default: ''
 		})
 		.property('where', {
 			type: 'string',
 			default: ''
+		})
+		.property('organiserName', {
+			type: 'string',
+			default: ''
+		})
+		.property('guests', {
+			type: 'array',
+			default: []
 		})
 		.property('createdAt', {
 			type: 'date'
